d54d03e60796b0f3d24f134848aedd430d95b215
Yi Li <leeoh12784@gmail.com>
2020-04-28 15:18:41 -0400

    uninterpreted functions with numeric details update

--- ila2_with_flex_numerics/uninterpreted_func.cc
+++ ila2_with_flex_numerics/uninterpreted_func.cc
@@ -7,32 +7,6 @@
 
 sc_biguint<8> flex_sim::signed_gt(sc_biguint<8> arg_0, sc_biguint<8> arg_1) {
 
-    /*
-  int arg_0_int = arg_0.to_int();
-  int arg_1_int = arg_1.to_int();
-
-  bool sb_0 = arg_0_int >> 7;
-  bool sb_1 = arg_1_int >> 7;
-
-  int abs_0 = arg_0_int & 127;
-  int abs_1 = arg_1_int & 127;
-
-  sc_biguint<8> result;
-
-  if (sb_0 && sb_1) {
-    result = (abs_0 < abs_1) ? arg_0 : arg_1;
-  } else if (sb_0 || sb_1) {
-    result = (sb_1) ? arg_0 : arg_1; 
-  } else {
-    result = (abs_0 > abs_1) ? arg_0 : arg_1;
-  }
-  */
-/*
-  sc_bigint<8> arg0_s = arg_0;
-  sc_bigint<8> arg1_s = arg_1;
-
-  sc_biguint<8> result = (arg0_s > arg1_s) ? arg_0 : arg_1;
-*/
   ac_int<8,false> arg_0_ac = arg_0.to_uint();
   ac_int<8,false> arg_1_ac = arg_1.to_uint();
   AdpfloatType<spec::kAdpfloatWordWidth,spec::kAdpfloatExpWidth> in_a = arg_0_ac;
@@ -48,32 +22,115 @@ sc_biguint<8> flex_sim::signed_gt(sc_biguint<8> arg_0, sc_biguint<8> arg_1) {
 
 sc_biguint<20> flex_sim::PEActEadd(sc_biguint<20> arg_0, sc_biguint<20> arg_1) {
   // TODO: need to implement signed Add
-  return arg_0;
+  sc_bigint<20> arg_0_s = arg_0;
+  sc_bigint<20> arg_1_s = arg_1;
+
+  ActScalarType arg_0_ac = arg_0_s.to_int();
+  ActScalarType arg_1_ac = arg_1_s.to_int();
+
+  ActScalarType out_tmp = arg_0_ac + arg_1_ac;
+
+  sc_bigint<20> result_s = out_tmp.to_int();
+  sc_biguint<20> result = result_s;
+  
+  return result;
 }
 
 sc_biguint<20> flex_sim::PEActEmul(sc_biguint<20> arg_0, sc_biguint<20> arg_1) {
   // TODO: implement signed multiply
-  return arg_0;
+  sc_bigint<20> arg_0_s = arg_0;
+  sc_bigint<20> arg_1_s = arg_1;
+  
+  ActScalarType arg_0_ac = arg_0_s.to_int();
+  ActScalarType arg_1_ac = arg_1_s.to_int();
+  
+  NVINTW(2*spec::kActWordWidth) tmp;
+  tmp = arg_0_ac * arg_1_ac;
+  tmp = nvhls::right_shift<NVINTW(2*spec::kActWordWidth)>(tmp, spec::kActNumFrac);
+  ActScalarType out_tmp = nvhls::get_slc<spec::kActWordWidth>(tmp, 0);
+
+  sc_bigint<20> result_s = out_tmp.to_int();
+  sc_biguint<20> result = result_s;
+
+  return result;
 }
 
 sc_biguint<20> flex_sim::PEActSigmoid(sc_biguint<20> arg_0) {
   // TODO: implement Sigmoid
-  return arg_0;
+  sc_bigint<20> arg_0_s = arg_0;
+  
+  ActScalarType arg_0_ac = arg_0_s.to_int();
+  ActScalarType out_tmp;
+
+  ac_fixed<spec::kActWordWidth, spec::kActNumInt, true, AC_TRN, AC_WRAP> in_ac; 
+  ac_fixed<spec::kActWordWidth, spec::kActNumInt, false, AC_TRN, AC_WRAP> out_ac;
+  
+  in_ac.set_slc(0, arg_0_ac);
+  out_ac = ac_math::ac_sigmoid_pwl
+            <ac_fixed<spec::kActWordWidth, spec::kActNumInt, false, AC_TRN, AC_WRAP> >(in_ac);
+
+  out_tmp.set_slc(0, nvhls::get_slc<spec::kActWordWidth>(out_ac, 0));
+
+  sc_bigint<20> result_s = out_tmp.to_int();
+  sc_biguint<20> result = result_s;
+
+  return result;
 }
 
 sc_biguint<20> flex_sim::PEActTanh(sc_biguint<20> arg_0) {
   // TODO: implement Tanh
-  return arg_0;
+  sc_bigint<20> arg_0_s = arg_0;
+
+  ActScalarType arg_0_ac = arg_0_s.to_int();
+  ActScalarType out_tmp;
+
+  ac_fixed<spec::kActWordWidth, spec::kActNumInt, true, AC_TRN, AC_WRAP> in_ac;
+  ac_fixed<spec::kActWordWidth, spec::kActNumInt, false, AC_TRN, AC_WRAP> out_ac;
+
+  in_ac.set_slc(0, arg_0_ac);
+
+  out_ac = ac_math::ac_tanh_pwl
+             <ac_fixed<spec::kActWordWidth, spec::kActNumInt, false, AC_TRN, AC_WRAP> >(in_ac);
+
+  out_tmp.set_slc(0, nvhls::get_slc<spec::kActWordWidth>(out_ac, 0));
+  
+  sc_bigint<20> result_s = out_tmp.to_int();
+  sc_biguint<20> result = result_s;
+
+  return result;
 }
 
 sc_biguint<20> flex_sim::PEActRelu(sc_biguint<20> arg_0) {
   // TODO: implement Relu
-  return arg_0;
+  sc_bigint<20> arg_0_s = arg_0;
+
+  ActScalarType arg_0_ac = arg_0_s.to_int();
+  ActScalarType out_tmp;
+
+  if (arg_0_ac < 0)
+      out_tmp = 0;
+  else 
+      out_tmp = arg_0_ac;
+
+  sc_bigint<20> result_s = out_tmp.to_int();
+  sc_biguint<20> result = result_s;
+
+  return result;
 }
 
 sc_biguint<20> flex_sim::PEActOnex(sc_biguint<20> arg_0) {
   // TODO: implement Onex
-  return arg_0;
+  sc_bigint<20> arg_0_s = arg_0;
+
+  ActScalarType arg_0_ac = arg_0_s.to_int();
+  ActScalarType out_tmp;
+
+  out_tmp = (1<<spec::kActNumFrac) - arg_0_ac;
+
+  sc_bigint<20> result_s = out_tmp.to_int();
+  sc_biguint<20> result = result_s;
+
+  return result;
 }
 
 sc_biguint<20> flex_sim::Adptflow2Fixed(sc_biguint<8> arg_0, sc_biguint<3> arg_1) {
