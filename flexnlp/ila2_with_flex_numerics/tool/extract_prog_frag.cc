4d8941e29357bb8fd9d0d08ed6253ff4e8e87fd8
Yi Li <leeoh12784@gmail.com>
2020-10-20 15:45:14 -0400

    update

---
+++ flexnlp/ila2_with_flex_numerics/tool/extract_prog_frag.cc
@@ -0,0 +1,14 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+
+#include "nlohmann/json.hpp"
+
+using namespace std;
+using json = nlohmann::json;
+
+int main(int argc, char* argv[]) {
+  
+}
